ca15f8e09a7fe1c3873e83e327e557e897a07a19
kaamil2 <kaamil22thobani@gmail.com>
2024-05-16 22:43:46 -0400

    working cli

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,38 +1,22 @@
 #!/usr/bin/env node
-import {Command} from 'commander';
-import inquirer from 'inquirer';
 
-const program = new Command();
+import inquirer from "inquirer";
 
-program
-  .command('generate-routes')
-  .description('Generate routes for your application')
-  .action(async () => {
-    const answers = await inquirer.prompt([
-        {
-          type: 'list',
-          name: 'backendLanguage',
-          message: 'What backend language are you using?',
-          choices: ['TypeScript', 'Python', 'Go'],
-        },
-        {
-          type: 'confirm',
-          name: 'deployDatabase',
-          message: 'Do you want to deploy a database to the cloud?',
-        },
-        {
-          type: 'input',
-          name: 'token',
-          message: 'Enter your cloud database token:'
-        },
-      ]) as { backendLanguage: string, deployDatabase: boolean, token?: string };
-      
-      console.log(`You chose ${answers.backendLanguage} with deployment: ${answers.deployDatabase}`);
-      if (answers.token) {
-        console.log(`Token: ${answers.token}`);
-      }
-      
-    // Additional logic to handle Prisma setup and schema initialization
-  });
-
-program.parse(process.argv);
+await inquirer.prompt([
+  {
+    type: "list",
+    name: "backendLanguage",
+    message: "What backend language are you using?",
+    choices: ["TypeScript", "Python", "Go"],
+  },
+  {
+    type: "confirm",
+    name: "deployDatabase",
+    message: "Do you want to deploy a database to the cloud?",
+  },
+  {
+    type: "input",
+    name: "token",
+    message: "Enter your cloud database token:",
+  },
+]);

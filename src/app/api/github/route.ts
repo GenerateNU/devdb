14ac78633af99e7249999e875710e5b2603d3f96
Nate Sawant <natesawant@gmail.com>
2024-05-22 02:25:39 -0400

    Get webhooks working

---
+++ src/app/api/github/route.ts
@@ -0,0 +1,54 @@
+import { Octokit } from "@octokit/rest";
+import { exec } from "child_process";
+import { promisify } from "util";
+import { writeFile } from "fs/promises";
+import { NextResponse } from "next/server";
+
+const execAsync = promisify(exec);
+
+// Initialize Octokit with an access token
+const octokit = new Octokit({
+  auth: process.env.GITHUB_ACCESS_TOKEN,
+});
+
+// To handle a POST request to /api
+export async function POST(request: {
+  ref: string;
+  repository: { owner: string; name: string };
+}) {
+  const input = request;
+
+  if (input.ref === "refs/heads/main") {
+    try {
+      // Fetch the content of the Prisma schema file using Octokit
+      const response = await octokit.repos.getContent({
+        owner: input.repository.owner,
+        repo: input.repository.name,
+        path: "prisma/schema.prisma",
+        mediaType: {
+          format: "raw",
+        },
+      });
+
+      const schemaContent = response.data as unknown as string;
+
+      // Save schemaContent to a local file
+      await writeFile("prisma/schema.prisma", schemaContent, "utf8");
+
+      // Apply changes using Prisma
+      const { stdout, stderr } = await execAsync("npx prisma db push");
+      console.log(stdout);
+      if (stderr) {
+        console.error("Error during database push:", stderr);
+        throw new Error("Failed to update database schema");
+      }
+      return { success: true };
+    } catch (error) {
+      console.error("Failed to handle GitHub push:", error);
+      throw new Error("Failed to handle GitHub push");
+    }
+  }
+
+  // Do whatever you want
+  return NextResponse.json({ ignored: true }, { status: 200 });
+}
---
+++ src/app/api/route.ts
@@ -0,0 +1,17 @@
+// app/api/routs.ts 👈🏽
+
+import { NextResponse } from "next/server";
+
+// To handle a GET request to /api
+export async function GET(request) {
+  // Do whatever you want
+  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
+
+// To handle a POST request to /api
+export async function POST(request) {
+  // Do whatever you want
+  return NextResponse.json({ message: "Hello World" }, { status: 200 });
+}
+
+// Same logic to add a `PATCH`, `DELETE`...

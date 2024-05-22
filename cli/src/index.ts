7edb03caece523929c4f1eb4ec682f597826a109
Nate Sawant <natesawant@gmail.com>
2024-05-22 12:41:46 -0400

    Fixed

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -9,25 +9,26 @@ import parseGithubUrl from "parse-github-url";
 import axios from "axios";
 import { readFile, writeFile } from "fs/promises";
 
-
-const baseUrl = "https://pig-content-happily.ngrok-free.app/";
+const baseUrl = "https://routes-orcin.vercel.app/";
 const apiPath = "api/trpc/";
 const webhookPath = "github.makeWebhook";
 const createDatabasePath = "database.create";
 const endpointPath = "database.endpoint";
 
-
-async function updateExistingEnvVariable(varName: string, newValue: string): Promise<Boolean> {
+async function updateExistingEnvVariable(
+  varName: string,
+  newValue: string,
+): Promise<boolean> {
   try {
-    // Path to the .env.example file
-    const filePath = './.env.example';
+    // Path to the .env file
+    const filePath = ".env";
     const content = await readFile(filePath);
 
     // Split the content into lines
-    const lines = content.toString().split('\n');
+    const lines = content.toString().split("\n");
 
     // Update the line containing the variable
-    const updatedLines = lines.map((line:string) => {
+    const updatedLines = lines.map((line: string) => {
       if (line.startsWith(varName)) {
         // Update the value of the variable
         return `${varName}=${newValue}`;
@@ -36,28 +37,28 @@ async function updateExistingEnvVariable(varName: string, newValue: string): Pro
     });
 
     // Join the updated lines back into a single string
-    const updatedContent = updatedLines.join('\n');
+    const updatedContent = updatedLines.join("\n");
 
     // Write the updated contents back to the .env.example file
     await writeFile(filePath, updatedContent);
     //potentially add utf8 above if it fails
-    const verifyContent = await readFile(filePath, 'utf8');
+    const verifyContent = await readFile(filePath, "utf8");
     return verifyContent.includes(`${varName}=${newValue}`);
   } catch (error) {
-    console.error('Failed to update .env.example:', error);
+    console.error("Failed to update .env.example:", error);
     return false;
   }
 }
 
 async function askRetry(): Promise<boolean> {
-  const answers = await inquirer.prompt([
-      {
-          type: 'confirm',
-          name: 'retry',
-          message: 'Do you want to try updating the environment variable again?',
-          default: false,
-      }
-  ]);
+  const answers = (await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "retry",
+      message: "Do you want to try updating the environment variable again?",
+      default: false,
+    },
+  ])) as { retry: boolean };
   return answers.retry;
 }
 
@@ -161,7 +162,7 @@ async function main() {
     console.log("Deploying, this may take up to 10 minutes");
 
     // Send request to backend to create database
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 120; i++) {
       try {
         const endpointResponse = await axios.post(
           `${baseUrl}${apiPath}${endpointPath}`,
@@ -173,26 +174,27 @@ async function main() {
         );
 
         if (endpointResponse) {
-          
           const endpointData = endpointResponse.data as EndpointResponse;
           console.log("Connection information:\n");
           console.log("\t" + endpointData.result.data.json.connection);
           console.log();
-          updateExistingEnvVariable('CONNECTION_URL', 'endpointData.result.data.json.connection');
-          //updateEnvironmentVariable('CONNECTION_URL', endpointData.result.data.json.connection);
-          // TODO: Automatically set connection as environment URL
-          const success = await updateExistingEnvVariable('CONNECTION_URL', endpointData.result.data.json.connection);
-        if (success) {
-          console.log('Environment variable updated successfully.');
-          updateSuccessful = await askRetry();
-        } else {
-          console.error('Failed to update environment variable.');
-          updateSuccessful = await askRetry();
-        }
+
+          // Automatically set connection as environment URL
+          const success = await updateExistingEnvVariable(
+            "DATABASE_URL",
+            endpointData.result.data.json.connection,
+          );
+          if (success) {
+            console.log("Environment variable updated successfully.");
+            updateSuccessful = await askRetry();
+          } else {
+            console.error("Failed to update environment variable.");
+            updateSuccessful = await askRetry();
+          }
           break;
         }
       } catch (error) {
-        await new Promise((resolve) => setTimeout(resolve, 120000));
+        await new Promise((resolve) => setTimeout(resolve, 10000));
         console.warn("Retrying...");
       }
     }

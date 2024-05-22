fc00643ff7d784a64fdab56abb602a3786d643c0
Nate Sawant <74106957+natesawant@users.noreply.github.com>
2024-05-22 12:52:57 -0400

    Add database URL to environment variable file (#15)
    * cli changes for the connection
    
    * Fixed
    
    * Append if doesn't exist
    
    ---------
    
    Co-authored-by: kaamil2 <kaamil22thobani@gmail.com>

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -7,12 +7,67 @@ import { execa } from "execa";
 import { getUserPkgRunner } from "./utils/getPackageManager";
 import parseGithubUrl from "parse-github-url";
 import axios from "axios";
+import { readFile, writeFile } from "fs/promises";
 
 const baseUrl = "https://routes-orcin.vercel.app/";
 const apiPath = "api/trpc/";
 const webhookPath = "github.makeWebhook";
 const createDatabasePath = "database.create";
 const endpointPath = "database.endpoint";
+
+async function updateExistingEnvVariable(
+  varName: string,
+  newValue: string,
+): Promise<boolean> {
+  try {
+    // Path to the .env file
+    const filePath = ".env";
+    const content = await readFile(filePath);
+
+    // Split the content into lines
+    const lines = content.toString().split("\n");
+
+    let missingVar = true;
+
+    // Update the line containing the variable
+    const updatedLines = lines.map((line: string) => {
+      if (line.startsWith(varName)) {
+        missingVar = false;
+        // Update the value of the variable
+        return `${varName}=${newValue}`;
+      }
+      return line;
+    });
+
+    if (missingVar) {
+      updatedLines.push(`${varName}=${newValue}`);
+    }
+
+    // Join the updated lines back into a single string
+    const updatedContent = updatedLines.join("\n");
+
+    // Write the updated contents back to the .env.example file
+    await writeFile(filePath, updatedContent);
+    //potentially add utf8 above if it fails
+    const verifyContent = await readFile(filePath, "utf8");
+    return verifyContent.includes(`${varName}=${newValue}`);
+  } catch (error) {
+    console.error("Failed to update .env.example:", error);
+    return false;
+  }
+}
+
+async function askRetry(): Promise<boolean> {
+  const answers = (await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "retry",
+      message: "Do you want to try updating the environment variable again?",
+      default: false,
+    },
+  ])) as { retry: boolean };
+  return answers.retry;
+}
 
 //this is where the cli code is generated to ensure we are able to get the user info
 async function main() {
@@ -113,7 +168,7 @@ async function main() {
     console.log("Deploying, this may take up to 10 minutes");
 
     // Send request to backend to create database
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 120; i++) {
       try {
         const endpointResponse = await axios.post(
           `${baseUrl}${apiPath}${endpointPath}`,
@@ -129,11 +184,26 @@ async function main() {
           console.log("Connection information:\n");
           console.log("\t" + endpointData.result.data.json.connection);
           console.log();
-          // TODO: Automatically set connection as environment URL
+
+          // Automatically set connection as environment URL
+          let retrySetEnv = true;
+          while (retrySetEnv) {
+            const success = await updateExistingEnvVariable(
+              "DATABASE_URL",
+              endpointData.result.data.json.connection,
+            );
+            if (success) {
+              console.log("Environment variable updated successfully.");
+              break;
+            } else {
+              console.error("Failed to update environment variable.");
+              retrySetEnv = await askRetry();
+            }
+          }
           break;
         }
       } catch (error) {
-        await new Promise((resolve) => setTimeout(resolve, 120000));
+        await new Promise((resolve) => setTimeout(resolve, 10000));
         console.warn("Retrying...");
       }
     }

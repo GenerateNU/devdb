45e25a845c59fe211024929a0223b78a722afc1b
Nate Sawant <natesawant@gmail.com>
2024-05-21 23:22:21 -0400

    Add AWS stuff

--- src/server/api/routers/databases.ts
+++ src/server/api/routers/databases.ts
@@ -1,13 +1,60 @@
 import { z } from "zod";
 
 import { publicProcedure } from "~/server/api/trpc";
-import { CreateDatabase } from "~/server/external/aws";
+import { CreateDatabase, GetDatabaseConnection } from "~/server/external/aws";
 import { DBProvider } from "~/server/external/types";
+import gitUrlParse from "git-url-parse";
 
-export const database = publicProcedure
-  .input(z.object({ name: z.string(), provider: z.nativeEnum(DBProvider) }))
-  .mutation(async ({ input }) => {
-    const result = await CreateDatabase(input.name, input.provider);
+export const database = {
+  create: publicProcedure
+    .input(
+      z.object({ repoUrl: z.string(), provider: z.nativeEnum(DBProvider) }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const parseResults = gitUrlParse(input.repoUrl);
 
-    return result;
-  });
+      const branch = "main";
+
+      const projectCount = await ctx.db.project.count({
+        where: {
+          repo: input.repoUrl,
+        },
+      });
+
+      if (projectCount == 0) {
+        await ctx.db.project.create({
+          data: { repo: input.repoUrl },
+        });
+      }
+
+      await ctx.db.database.create({
+        data: {
+          branch: branch,
+          projectRepo: input.repoUrl,
+        },
+      });
+
+      const databaseName = `${parseResults.owner}-${parseResults.name}-${branch}`;
+
+      const result = await CreateDatabase(databaseName, input.provider);
+
+      return result;
+    }),
+
+  endpoint: publicProcedure
+    .input(z.object({ repoURL: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const dbResults = await ctx.db.database.findFirstOrThrow({
+        select: {
+          id: true,
+        },
+        where: {
+          projectRepo: input.repoURL,
+        },
+      });
+
+      const result = await GetDatabaseConnection(dbResults.id);
+
+      return result;
+    }),
+};
--- src/server/external/aws.ts
+++ src/server/external/aws.ts
@@ -3,6 +3,7 @@ import {
   CreateDBInstanceCommand,
   DescribeDBInstancesCommand,
   DBInstanceNotFoundFault,
+  type CreateDBInstanceCommandOutput,
 } from "@aws-sdk/client-rds";
 import type { DBProvider } from "./types";
 
@@ -11,7 +12,7 @@ const client = new RDSClient({ region: "us-east-1" });
 export async function CreateDatabase(
   name: string,
   provider: DBProvider,
-): Promise<string> {
+): Promise<CreateDBInstanceCommandOutput> {
   const commandInput = {
     AllocatedStorage: 20,
     DBInstanceClass: "db.t3.micro",
@@ -21,62 +22,47 @@ export async function CreateDatabase(
     MasterUserPassword: "devpassword123",
   };
   const command = new CreateDBInstanceCommand(commandInput);
-  const result = await client.send(command);
+  const result = client.send(command);
 
-  try {
-    const endpoint = await GetDatabaseConnection(
-      result.DBInstance?.DBInstanceIdentifier,
-    );
-    if (endpoint) return endpoint;
-    else throw Error("Problem getting connection detail.");
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+  return result;
 }
 
-async function GetDatabaseConnection(
+export async function GetDatabaseConnection(
   instanceId: string | undefined,
 ): Promise<string | undefined> {
-  const retries = 10;
-  const retryInterval = 120000;
   const input = {
     // DescribeDBInstancesMessage
     DBInstanceIdentifier: instanceId,
   };
   const command = new DescribeDBInstancesCommand(input);
 
-  for (let i = 0; i < retries; i++) {
-    console.log(`Attempt #${i}`);
-    try {
-      const response = await client.send(command);
+  try {
+    const response = await client.send(command);
 
-      if (response.DBInstances) {
-        if (response.DBInstances?.length < 1) {
-          throw Error("Database not found");
-        } else if (response.DBInstances[0]?.DBInstanceStatus == "Available") {
-          const provider = response.DBInstances[0].Engine;
-          const username = "dev";
-          const password = "devpassword123";
-          const awsEndpoint = response.DBInstances[0].Endpoint?.Address; //"test-database.cw6wi7ttmo36.us-east-1.rds.amazonaws.com";
-          const port = response.DBInstances[0].Endpoint?.Port;
-          const dbName = response.DBInstances[0].DBName;
+    if (response.DBInstances) {
+      if (response.DBInstances?.length < 1) {
+        throw Error("Database not found");
+      } else if (response.DBInstances[0]?.DBInstanceStatus == "Available") {
+        const provider = response.DBInstances[0].Engine;
+        const username = "dev";
+        const password = "devpassword123";
+        const awsEndpoint = response.DBInstances[0].Endpoint?.Address; //"test-database.cw6wi7ttmo36.us-east-1.rds.amazonaws.com";
+        const port = response.DBInstances[0].Endpoint?.Port;
+        const dbName = response.DBInstances[0].DBName;
 
-          const connection = `${provider}://${username}:${password}@${awsEndpoint}:${port}/${dbName}`;
+        const connection = `${provider}://${username}:${password}@${awsEndpoint}:${port}/${dbName}`;
 
-          return connection;
-        } else {
-          console.log("retrying...");
-          await new Promise((resolve) => setTimeout(resolve, retryInterval));
-        }
-      }
-    } catch (error) {
-      if (error instanceof DBInstanceNotFoundFault) {
-        throw error;
+        return connection;
       } else {
-        console.error(error);
-        // Sleep before retry
+        throw Error("Database not yet available");
       }
+    }
+  } catch (error) {
+    if (error instanceof DBInstanceNotFoundFault) {
+      throw error;
+    } else {
+      console.error(error);
+      // Sleep before retry
     }
   }
 }

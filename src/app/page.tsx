509cf44fd62097b387566ebee10de5849b4449ff
Nate Sawant <natesawant@gmail.com>
2024-05-22 15:28:06 -0400

    Remove unused variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 
-import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 

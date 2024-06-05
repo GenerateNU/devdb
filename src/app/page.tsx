d02d9a5c4cb51778608c002d072ba56990a0ab09
Nate Sawant <natesawant@gmail.com>
2024-06-05 17:19:14 -0400

    Fix env var and other lint errors

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getServerAuthSession } from "~/server/auth";
-import SignInScreen from "./_components/SignInScreen";
-import Dashboard from "./_components/Dashboard";
-
-export default async function HomePage() {
-  const session = await getServerAuthSession();
-  return <>{session ? <Dashboard /> : <SignInScreen />}</>;
-}
